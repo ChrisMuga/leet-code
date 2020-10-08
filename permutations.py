7ed2c0a5236c5865852b2844b23fcd031441b18f
Chris Muga <chrismuga94@gmail.com>
2020-10-08 13:07:06 +0300

    Permutations

--- permutations.py
+++ permutations.py
@@ -4,10 +4,21 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int])->List[List[int]]:
-        return nums
+        result = []
+        for idx, num in enumerate(nums):
+            for idx_, num_ in enumerate(nums):
+                copy_ = nums.copy()
+                if idx != idx_:
+                    copy_[idx_] = num
+                    copy_[idx] = num_
+                    result.append(copy_)
+            break
+
+        return(result)
 
 
 solution = Solution()
+
 
 input_nums = [1, 2, 3]
 ans = solution.permute(input_nums)
